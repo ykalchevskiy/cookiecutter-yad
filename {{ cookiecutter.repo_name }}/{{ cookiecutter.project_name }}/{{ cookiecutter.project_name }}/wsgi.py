b3aa2ebde8faaa4adc064bbb9d0c038455be41f1
Yan Kalchevksiy <yan.kalchevskiy@gmail.com>
2014-08-09 00:14:47 +0700

    Replaced local settings with dev ones.

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/wsgi.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/wsgi.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings.dev")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
